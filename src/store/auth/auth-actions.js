a72b7073a92f9078f88f50867bcab9e8fedd1887
Yigit Er <mr.yigiter222@gmail.com>
2023-10-08 20:27:26 +0300

    v1

--- src/store/auth/auth-actions.js
+++ src/store/auth/auth-actions.js
@@ -1,15 +1,14 @@
 import { LOGIN_FAIL, LOGIN_SUCCESS, LOGOUT } from "../types";
 
 export const loginSuccess = (user) => ({
-    type:LOGIN_SUCCESS ,
-    payload:user,
+	type: LOGIN_SUCCESS,
+	payload: user,
 });
 
 export const loginFail = () => ({
-    type:LOGIN_FAIL ,
+	type: LOGIN_FAIL,
 });
 
 export const logout = () => ({
-    type:LOGOUT ,
+	type: LOGOUT,
 });
-
--- src/store/counter/counter-actions.js
+++ src/store/counter/counter-actions.js
@@ -1,13 +1,6 @@
 import { COUNTER_DOWN, COUNTER_SET, COUNTER_UP } from "../types";
 
+export const counterUp = () => ({ type: COUNTER_UP });
+export const counterDown = () => ({ type: COUNTER_DOWN });
 
-export const counterUp =() =>(
-    {type:COUNTER_UP}
-)
-export const counterDown=() =>(
-    {type:COUNTER_DOWN}
-)
-
-export const counterSet =(value) =>(
-    {type:COUNTER_SET, payload:value}
-)
+export const counterSet = (value) => ({ type: COUNTER_SET, payload: value });
